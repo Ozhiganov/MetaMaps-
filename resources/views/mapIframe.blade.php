46583c039030dc67e045212af765104593b8d837
Dominik Pfennig <dominik@suma-ev.de>
2016-11-15 07:42:53 +0100

    Erster Schritt für eine Anzeige im IFrame

---
+++ resources/views/mapIframe.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8"/>
+        <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+        <meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport"/>
+        <title>
+            Maps - MetaGer
+        </title>
+        <link href="/css/all.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <main>
+            <div class="ol-popup" id="popup">
+                <a class="ol-popup-closer" href="#" id="popup-closer">
+                </a>
+                <div id="popup-content">
+                </div>
+            </div>
+            <div class="map" id="map">
+            </div>
+            <div class="hidden" id="closer" title="Ergebnisse einklappen">
+                >
+            </div>
+            <div class="col-xs-11 col-sm-6 col-md-3 hidden" data-status="in" id="results">
+            </div>
+        </main>
+        <script src="/js/all.js">
+        </script>
+    </body>
+</html>
