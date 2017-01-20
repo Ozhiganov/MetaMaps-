7877cc76c3b47f95a869dec2b7aad82e47d99e77
Dominik Pfennig <dominik@suma-ev.de>
2017-01-20 14:46:16 +0100

    Routen Assistent aktualisiert nicht mehr so häufig und startet auch nicht mehr mehrere Aktualisierungen gleichzeitig

--- public/js/routing.js
+++ public/js/routing.js
@@ -93726,31 +93726,59 @@ function prepareInterface() {
 function deinitAssistent() {
     window.location.reload();
 }
+var currentPosition = {};
+var calculating = false;
 
 function startLocationFollowing() {
     if (followingId === null) {
+        options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
         followingId = navigator.geolocation.watchPosition(function(position) {
             var timestamp = Math.floor(position.timestamp / 1000);
             var lon = parseFloat(position.coords.longitude);
             var lat = parseFloat(position.coords.latitude);
             var accuracy = position.coords.accuracy;
-            // We cancel if the coordinates are already in this array
-            var contained = false;
-            $.each(positions, function(index, value) {
-                if (lon === value.lon && lat === value.lat) {
-                    contained = true;
-                    return false;
-                }
-            });
-            if (!contained) {
-                positions.push({
-                    timestamp: timestamp,
-                    lon: lon,
-                    lat: lat,
-                    accuracy: accuracy
-                });
+            var newPosition = {
+                timestamp: timestamp,
+                lon: lon,
+                lat: lat,
+                accuracy: accuracy
+            };
+            var dist = getDistance(currentPosition, newPosition);
+            var minDist = 0;
+            switch (vehicle) {
+                case "car":
+                    minDist = 20;
+                    break;
+                case "bicycle":
+                    minDist = 10;
+                    break;
+                case "foot":
+                    minDist = 5;
+                    break;
             }
-            if (positions.length >= 2 && !contained) {
+            if (dist <= minDist || calculating) {
+                console.log(dist, minDist, calculating);
+                return;
+            } else {
+                calculating = true;
+            }
+            currentPosition = {
+                timestamp: timestamp,
+                lon: lon,
+                lat: lat,
+                accuracy: accuracy
+            };
+            positions.push({
+                timestamp: timestamp,
+                lon: lon,
+                lat: lat,
+                accuracy: accuracy
+            });
+            if (positions.length >= 2) {
                 // Generate url to retrieve
                 var positionstring = "";
                 var timestampstring = "";
@@ -93763,18 +93791,17 @@ function startLocationFollowing() {
                 positionstring = positionstring.replace(/;+$/, '');
                 timestampstring = timestampstring.replace(/;+$/, '');
                 radiusstring = radiusstring.replace(/;+$/, '');
-
-                var url = '/route/match/'+vehicle+'/'+positionstring+'/'+timestampstring+'/'+radiusstring;
+                var url = '/route/match/' + vehicle + '/' + positionstring + '/' + timestampstring + '/' + radiusstring;
                 $.getJSON(url, function(r) {
                     // Aus den "gesnappten" Koordinaten berechnen wir nun die gefahrene Route
                     if (r.code === "Ok") {
                         var position = {
-                            hint: r.tracepoints[r.tracepoints.length - 1].hint,
-                            lon: parseFloat(r.tracepoints[r.tracepoints.length - 1].location[0]),
-                            lat: parseFloat(r.tracepoints[r.tracepoints.length - 1].location[1])
-                        }
-                        // Wir nehmen vorerst immer das Beste Matching für die gefahrene Route.
-                        // Vielleicht fällt mir später etwas besseres ein
+                                hint: r.tracepoints[r.tracepoints.length - 1].hint,
+                                lon: parseFloat(r.tracepoints[r.tracepoints.length - 1].location[0]),
+                                lat: parseFloat(r.tracepoints[r.tracepoints.length - 1].location[1])
+                            }
+                            // Wir nehmen vorerst immer das Beste Matching für die gefahrene Route.
+                            // Vielleicht fällt mir später etwas besseres ein
                         if (r.matchings !== null && r.matchings[0] !== undefined) {
                             // Clear the shown Route
                             routeAssistentVectorSource.clear();
@@ -93803,31 +93830,25 @@ function startLocationFollowing() {
                             if (hintsComplete) {
                                 url += "/" + hintString;
                             }
-                            $.getJSON(url, function(data){
-                                if(data.code === "Ok"){
+                            $.getJSON(url, function(data) {
+                                if (data.code === "Ok") {
                                     drawGeojson(data.routes[0].geometry, 'red');
-
                                     // Add the information for the next steps and route meta data
                                     // Route Metadata
                                     var duration = parseFloat(data.routes[0].duration);
                                     duration = parseDuration(duration)
                                     $("nav #route-information #duration").html(duration);
-
                                     var distance = parseFloat(data.routes[0].distance)
                                     distance = parseDistance(distance);
                                     $("nav #route-information #length").html(distance);
-
                                     // Next Step
                                     var step = data.routes[0].legs[0].steps[1];
                                     var stepString = parseManeuver(step["maneuver"], data.routes[0], 0, 1);
                                     $("nav #routing-steps .step-string").html(stepString);
-
                                     var img = parseImg(step);
                                     $("nav #routing-steps .step-image img").attr('src', img);
-
                                     var stepDistance = parseDistance(parseFloat(data.routes[0].legs[0].steps[0].distance));
                                     $("nav #routing-steps .step-length").html(stepDistance);
-
                                     // So now that everything is Drawn we adjust the Map View
                                     // The new Rotation is The bearing of the first step of the route:
                                     var rotation = parseInt(data.routes[0].legs[0].steps[0].maneuver.bearing_after);
@@ -93837,19 +93858,39 @@ function startLocationFollowing() {
                                     rotation /= 180;
                                     map.getView().setRotation(rotation);
                                     map.getView().setZoom(18);
-                                    map.getView().setCenter(ol.proj.transform([position.lon,position.lat], 'EPSG:4326', 'EPSG:3857'));
+                                    map.getView().setCenter(ol.proj.transform([position.lon, position.lat], 'EPSG:4326', 'EPSG:3857'));
                                 }
+                            }).always(function() {
+                                calculating = false;
                             });
+                        }else{
+                            calculating = false;
                         }
+                    }else{
+                        calculating = false;
                     }
                 });
+            }else{
+                calculating = false;
             }
         }, function(error) {
             // Follow Location couldn't be started. Abort now
             deinitAssistent();
-        });
+        }, options);
     }
 }
+var rad = function(x) {
+    return x * Math.PI / 180;
+};
+var getDistance = function(p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.lat - p1.lat);
+    var dLong = rad(p2.lon - p1.lon);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+};
 
 function removeRoute() {
     if (routeLayer !== null) {
