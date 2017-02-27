64925b1fed139d208c9cfb2743af5af2222e10e2
Dominik Pfennig <dominik@suma-ev.de>
2017-02-27 10:42:25 +0100

    TextÄnderung

--- resources/views/routen-assistent.blade.php
+++ resources/views/routen-assistent.blade.php
@@ -13,8 +13,8 @@
   <div class="panel-body">
     <p>Voraussetzung hierfür ist ein halbwegs aktueller mobiler Browser (z.B. Firefox - zu finden im App-Store)</p>
     <p>Plant man nun eine Route zu verschiedenen Wegpunkten, muss der Startpunkt die aktuelle <a href="/hilfe/gps">GPS-Position</a> sein Bei eingeschaltetem GPS auf dem Gerät erscheint eine entsprechende Option wenn man die Eingabe für den Wegpunkt startet).</p>
-    <p>Alle weiteren Wegpunkte können wie gewohnt beliebig entweder über die Suchfunktion innerhalb der Eingabebox für Wegpunkte im Routenplaner, oder durch ein Klick auf die Karte hinzugefügt werden</p>
-    <p>Nach einem Klick auf "Route berechnen" erscheint nun die Wegbeschreibung und zustätzlich darüber ein Knopf "Routenführung starten".</p>
+    <p>Alle weiteren Wegpunkte können wie gewohnt beliebig entweder über die Suchfunktion innerhalb der Eingabebox für Wegpunkte im Routenplaner, oder durch einen Klick auf die Karte hinzugefügt werden</p>
+    <p>Nach einem Klick auf "Route berechnen" erscheint nun die Wegbeschreibung und zusätzlich darüber ein Knopf "Routenführung starten".</p>
     <p>Ein Klick auf diesen Knopf startet dann die Navigation.</p>
   </div>
 </div>
