662110cc63d128ab2240377bb7389fcd860d9cb0
Dominik Pfennig <dominik@suma-ev.de>
2017-11-27 10:53:09 +0100

    MinZoom für die Karte vorest doch wieder auf 6 gesetzt
    Wir brauchen noch Änderungen in dem Tile Style damit die Zoomstufen 4 und 5 gut funktionieren.

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -111,7 +111,7 @@ InteractiveMap.prototype.initMap = function() {
         ]),
         view: new ol.View({
             maxZoom: 18,
-            minZoom: 4,
+            minZoom: 6,
             center: ol.proj.transform(
                 //[9.45824, 52.48812], 'EPSG:4326', 'EPSG:3857'),
                 initPos, 'EPSG:4326', 'EPSG:3857'),
